65a2a6a9f172fe9a6bc86a3b02bd259f9256102f
Hamdy Abd El Fattah <43134132+HamdyAbdELFatah@users.noreply.github.com>
2022-07-10 16:26:48 +0200

    create login screen

---
+++ app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -0,0 +1,43 @@
+package com.udacity.shoestore.ui.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil
+            .inflate< FragmentLoginBinding >(
+                inflater,
+                R.layout.fragment_login,
+                container,
+                false
+            )
+            return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+    }
+
+
+
+}
