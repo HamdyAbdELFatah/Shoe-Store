b483b80624ae400987e6a6cfcb2ba8aab5501b40
Hamdy Abd El Fattah <43134132+HamdyAbdELFatah@users.noreply.github.com>
2022-07-12 17:24:36 +0200

    setup toolbar with navigation

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,34 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Timber.plant(Timber.DebugTree())
+        navController = (supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+        setSupportActionBar(binding.toolbar)
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
